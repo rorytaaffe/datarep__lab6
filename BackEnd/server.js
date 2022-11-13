b105550d9411f88bf4d72170e9fad69319d7954d
rorytaaffe <g00332281@atu.ie>
2022-11-13 15:25:18 +0000

    First Commit

---
+++ BackEnd/server.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const app = express()
+const port = 4000
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+res.header("Access-Control-Allow-Headers",
+"Origin, X-Requested-With, Content-Type, Accept");
+next();
+});
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.get('/api/books', (req, res) => {
+
+    books:[
+        {
+        "title": "Learn Git in a Month of Lunches",
+        "isbn": "1617292419",
+        "pageCount": 0,
+        "thumbnailUrl":
+        "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg", "authors": ["Rick Umali"],
+        "categories": []
+        },
+        {
+            "title": "MongoDB in Action, Second Edition",
+            "isbn": "1617291609",
+            "pageCount": 0,
+            "thumbnailUrl":
+            "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
+            "status": "MEAP",
+            "authors": [
+            "Kyle Banker",
+            "Peter Bakkum",
+            "Tim Hawkins",
+            "Shaun Verch",
+            "Douglas Garrett"
+            ],
+            "categories": []    
+        },
+        {
+            "title": "Getting MEAN with Mongo, Express, Angular, and Node",
+            "isbn": "1617292036",
+            "pageCount": 0,
+            "thumbnailUrl":
+            "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
+            "status": "MEAP",
+            "authors": ["Simon Holmes"],
+            "categories": []
+            }
+    ];
+    res.status(200).json({
+        message: "Everything is ok",
+        books:books});
+})
+
+app.post('/api/books', (res, res)=>{
+    console.log('Book Received!');
+    console.log(req.body.title);
+    console.log(req.body.year);
+    console.log(req.body.poster);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
